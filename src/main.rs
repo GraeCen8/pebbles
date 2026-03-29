b63daea97d8cb415ccb7c92e65c6742e964cd0b8
grae ceney <gceney7@gmail.com>
2026-03-29 16:42:38 +0100

    added it to main to include it into the pipeline

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,19 @@
 fn main() {
     let src = std::fs::read_to_string("example.pbl").expect("read example.pbl");
+
     let mut lexer = pebbles::lexer::Lexer::new(&src);
     let tokens = lexer.tokenize();
+
     let mut parser = pebbles::parser::Parser::new(tokens);
-    let _ast = parser.parse().expect("parse example.pbl");
+    let ast = parser.parse().expect("parse example.pbl");
+
+    let mut checker = pebbles::typeck::TypeChecker;
+    checker.check(&ast).unwrap_or_else(|e| {
+        eprintln!("error {}" e)
+        std::process::exit(1);
+    })
+
+    //error checking 
+
+    //codegen 
 }
