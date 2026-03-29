754621a71ab5f43e72351b8ef3a31f2dee1a9678
grae ceney <gceney7@gmail.com>
2026-03-29 16:08:17 +0100

    added a main func to tie all 3 parts together

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    let src = std::fs::read_to_string("example.pbl").expect("read example.pbl");
+    let mut lexer = pebbles::lexer::Lexer::new(&src);
+    let tokens = lexer.tokenize();
+    let mut parser = pebbles::parser::Parser::new(tokens);
+    let _ast = parser.parse().expect("parse example.pbl");
 }
